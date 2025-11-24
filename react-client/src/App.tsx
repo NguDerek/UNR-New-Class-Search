d0e78f61d5a40113026b0cb2850873d9a8cc39cd
John Michael Libed <agasjohnmichael@gmail.com>
2025-11-24 15:52:46 -0800

    account creation moved to -> views components for functions, images, and graphics dedicated web pages to -> views

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import AccountCreation from './components/AccountCreation'
+import AccountCreation from './views/AccountCreation'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
