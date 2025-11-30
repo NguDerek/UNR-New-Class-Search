7c99e3f9818073efcab7bf28ce378dd6200b4526
John Michael Libed <agasjohnmichael@gmail.com>
2025-11-29 22:58:16 -0800

    forgot string email is deprecated again, fixed with new method

--- react-client/src/views/Login.tsx
+++ react-client/src/views/Login.tsx
@@ -3,6 +3,6 @@ import { useState } from 'react';
 
 //For Form Validation
 const loginSchema = z.object({
-  email: z.string().email('Invalid email address'),
+  email: z.email('Invalid email address'),
   password: z.string().min(1, 'Password is required'),
 });
