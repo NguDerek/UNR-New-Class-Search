9f818013a98196ff97c91415308f05fbcd15e649
John Michael Libed <agasjohnmichael@gmail.com>
2026-04-19 20:55:09 -0700

    Server Changes - Changed port to 465 as given VM does not like 587 and pings indefinitely so probably a network outbound issue, corresponding changes to TLS and SSL cerf as well.

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -36,8 +36,9 @@ app.config['SESSION_COOKIE_SECURE'] = False #False During Development #True in t
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
